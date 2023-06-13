f8e8119c466621ce02876242a12975a4266db28a
body08120 <nataelrenollet08120@gmail.com>
2023-06-13 15:45:01 +0200

    ajout divers texte, remplacement section valeurs index

--- service.php
+++ service.php
@@ -33,7 +33,7 @@
   <link rel="stylesheet" type="text/css" href="css/responsive.css" />
 
   <!-- Slider -->
-  <link rel="stylesheet" type="text/css" href="css/slider.css"/>
+  <link rel="stylesheet" type="text/css" href="css/slider.css" />
 
 </head>
 
@@ -53,7 +53,7 @@
 page-title-->
 
     <section class="page-title bg-overlay-black-60 jarallax" data-speed="0.6">
-    <div class="head-slider"><img src="img/02.jpg" alt=""></div>
+      <div class="head-slider"><img src="img/02.jpg" alt=""></div>
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
@@ -62,7 +62,8 @@ page-title-->
               <p>Jamais Seul ... </p>
             </div>
             <ul class="page-breadcrumb">
-              <li><a href="index.php"><i class="fa fa-home"></i> Accueil</a> <i class="fa fa-angle-double-right"></i></li>
+              <li><a href="index.php"><i class="fa fa-home"></i> Accueil</a> <i class="fa fa-angle-double-right"></i>
+              </li>
               <li><span>Nos Établissements et Services</span> </li>
             </ul>
           </div>
@@ -82,73 +83,78 @@ page-title -->
         <div class="row justify-content-center">
           <div class="col-lg-8">
             <div class="section-title text-center">
-            <h6>En savoir plus sur nos différents pôles et services </h6>
-            <h2 class="title-effect">Nos Différents Pôles</h2>
-          </div>
-    </div>
-    <!-- ============================================ -->
-   <div class="service-3">
-    <div class="row">
-      <div class="col-lg-6 col-md-6 position-relative">
-        <div class="service-blog text-end">
-          <h3 class="theme-color">Pôle Hébergement</h3>
-            <a href="hebergement.php">
-              <p>Au sein de notre centre, nous proposons des places d'hébergement et de réinsertion sociale (CHRS) qui offrent un environnement sûr et sécurisé aux individus et aux familles qui ont besoin d'un soutien temporaire.</p>
-            </a>
-              <b>01</b>
-              <ul class="list list-unstyled">
-                <li>Centre d'hébergement et de réinsertion sociale (CHRS) </li>
-                <li>Hébergement d'urgence (HU)</li>
-                <!-- <li>Custom web design</li> -->
-              </ul>
-          </div>
-       </div>
-       <div class="col-lg-6 col-md-6 xs-mt-30 xs-mb-30">
-         <img class="img-fluid full-width" src="img/s01.jpg" alt="">
-       </div>
-    </div>
-    <!-- ============================================ -->
-    <div class="row">
-
-     <div class="col-lg-6 col-md-6 position-relative">
-         <img class="img-fluid full-width" src="img/s02.jpg" alt="">
-       </div>
-        <div class="col-lg-6 col-md-6 xs-mt-30 xs-mb-30 position-relative">
-          <div class="service-blog left text-start">
-            <h3 class="theme-color">Pôle Médico-Social et Logement Adapté </h3>
-            <a href="medicosocial.php">
-              <p>Le pôle médico-social et logement adapté offre une gamme de services essentiels pour répondre aux besoins des personnes en situation de handicap et de troubles de santé mentale.</p>
-            </a>
-              <b>02</b>
-              <ul class="list list-unstyled">
-                <li>ACT (+ dossier de demande) </li>
-                <li>LHSS (+ dossier de demande) </li>
-                <li>Maison relais / Pension de famille</li>
-              </ul>
-          </div>
-       </div>
-    </div>
-    <!-- ============================================ -->
-    <div class="row">
-       <div class="col-lg-6 col-md-6 xs-mt-30 xs-mb-30 position-relative">
-          <div class="service-blog text-end">
-            <a href="asile.php">
-             <h3 class="theme-color">Pôle Asile</h3>
-              <p></p>
-              <b>03</b>
-              <ul class="list list-unstyled">
-                <li>Centres d’accueil pour demandeurs d’asile (CADA)</li>
-                <li>Hébergement d’urgence des demandeurs d’asile (HUDA)</li>
-                <li>Centre d’accueil et d’examen des situations (CAES)</li>
-              </ul>
-            </a>
+              <h6>En savoir plus sur nos différents pôles et services </h6>
+              <h2 class="title-effect">Nos Différents Pôles</h2>
             </div>
+          </div>
           <!-- ============================================ -->
-        </div>
-        <div class="col-lg-6 col-md-6 xs-mt-30 xs-mb-30">
-          <img class="img-fluid full-width" src="img/s03.jpg" alt="">
-        </div>
-      </div>
+          <div class="service-3">
+            <div class="row">
+              <div class="col-lg-6 col-md-6 position-relative">
+                <div class="service-blog text-end">
+                  <h3 class="theme-color">Pôle Hébergement</h3>
+                  <a href="hebergement.php">
+                    <p>Au sein de notre centre, nous proposons des places d'hébergement et de réinsertion sociale (CHRS)
+                      qui offrent un environnement sûr et sécurisé aux individus et aux familles qui ont besoin d'un
+                      soutien temporaire.</p>
+                  </a>
+                  <b>01</b>
+                  <ul class="list list-unstyled">
+                    <li>Centre d'hébergement et de réinsertion sociale (CHRS) </li>
+                    <li>Hébergement d'urgence (HU)</li>
+                    <!-- <li>Custom web design</li> -->
+                  </ul>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 xs-mt-30 xs-mb-30">
+                <img class="img-fluid full-width" src="img/s01.jpg" alt="">
+              </div>
+            </div>
+            <!-- ============================================ -->
+            <div class="row">
+
+              <div class="col-lg-6 col-md-6 position-relative">
+                <img class="img-fluid full-width" src="img/s02.jpg" alt="">
+              </div>
+              <div class="col-lg-6 col-md-6 xs-mt-30 xs-mb-30 position-relative">
+                <div class="service-blog left text-start">
+                  <h3 class="theme-color">Pôle Médico-Social et Logement Adapté </h3>
+                  <a href="medicosocial.php">
+                    <p>Le pôle médico-social et logement adapté offre une gamme de services essentiels pour répondre aux
+                      besoins des personnes en situation de handicap et de troubles de santé mentale.</p>
+                  </a>
+                  <b>02</b>
+                  <ul class="list list-unstyled">
+                    <li>ACT (+ dossier de demande) </li>
+                    <li>LHSS (+ dossier de demande) </li>
+                    <li>Maison relais / Pension de famille</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <!-- ============================================ -->
+            <div class="row">
+              <div class="col-lg-6 col-md-6 xs-mt-30 xs-mb-30 position-relative">
+                <div class="service-blog text-end">
+                  <a href="asile.php">
+                    <h3 class="theme-color">Pôle Asile</h3>
+                    <p>Le Pôle Asile est une structure qui vise à fournir une assistance et un soutien aux demandeurs
+                      d'asile tout au long de leur parcours. Il comprend plusieurs types de centres d'accueil adaptés
+                      aux besoins spécifiques des personnes en quête de protection internationale.</p>
+                    <b>03</b>
+                    <ul class="list list-unstyled">
+                      <li>Centres d’accueil pour demandeurs d’asile (CADA)</li>
+                      <li>Hébergement d’urgence des demandeurs d’asile (HUDA)</li>
+                      <li>Centre d’accueil et d’examen des situations (CAES)</li>
+                    </ul>
+                  </a>
+                </div>
+                <!-- ============================================ -->
+              </div>
+              <div class="col-lg-6 col-md-6 xs-mt-30 xs-mb-30">
+                <img class="img-fluid full-width" src="img/s03.jpg" alt="">
+              </div>
+            </div>
     </section>
 
     <!--=================================
