6846076373339af8dd1f354ac99e7b5852b997ed
body08120 <nataelrenollet08120@gmail.com>
2023-07-24 16:18:09 +0200

    fix index

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+    }
+    
 require_once('src/controllers/homeController.php');
 
 if (isset($_GET['action']) && $_GET['action'] !== '' && !isset($_GET['admin'])) {
