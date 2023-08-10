e8af1728c33ab065236ca417de516a7edf80bebe
body08120 <nataelrenollet08120@gmail.com>
2023-08-10 11:37:00 +0200

    filtrage donnee ok

--- src/php/admin/treatmentLogin.php
+++ src/php/admin/treatmentLogin.php
@@ -1,28 +1,48 @@
 <?php
+// Inclure votre UserRepository ici
 $userRepo = new UserRepository();
-$email = htmlspecialchars(strip_tags(trim($_POST['email'])));
-$username = htmlspecialchars(strip_tags(trim($_POST['username'])));
-$password = htmlspecialchars(strip_tags(trim($_POST['password'])));
 
+// Valider et nettoyer les entrées utilisateur
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+$username = trim($_POST['username']);
+$password = $_POST['password'];
 
+// Vérifier si les champs obligatoires sont remplis
+if (empty($username) || empty($password)) {
+    $_SESSION['error-message'] = "Nom d'utilisateur et mot de passe requis.";
+    header('Location: index.php?action=Connexion');
+    exit;
+}
+
+// Vérifier si l'email est valide
+if ($email === false) {
+    $_SESSION['error-message'] = "Adresse email non valide.";
+    header('Location: index.php?action=Connexion');
+    exit;
+}
+
+// Rechercher l'utilisateur par email et nom d'utilisateur
 $user = $userRepo->getUserByEmailAndUsername($email, $username);
-if ($user != []) {
-    if (password_verify($password, $user->getPassword())) {
 
+// Vérifier si un utilisateur a été trouvé
+if ($user !== null) {
+    // Vérifier le mot de passe
+    if (password_verify($password, $user->getPassword())) {
+        // Authentification réussie
         $_SESSION['username'] = $user->getUsername();
         $_SESSION['id_user'] = $user->getIdUser();
-        $_SESSION['success-message'] = "Bienvenue sur votre espace administrateur !";
+        $_SESSION['success-message'] = "Connexion réussie.";
         header('Location: index.php?admin&action=');
         exit;
     } else {
-
-        $_SESSION['error-message'] = "Un des champs est incorrect.";
+        // Mot de passe incorrect
+        $_SESSION['error-message'] = "Erreur de connexion.";
         header('Location: index.php?action=Connexion');
         exit;
     }
 } else {
-
-    $_SESSION['error-message'] = "Aucun utilisateur n'a été trouvé.";
+    // Aucun utilisateur trouvé
+    $_SESSION['error-message'] = "Erreur de connexion.";
     header('Location: index.php?action=Connexion');
     exit;
 }
