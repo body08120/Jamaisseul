8df0d663720c7bd0c1f5e67ec9e24e24e82634f0
body08120 <nataelrenollet08120@gmail.com>
2023-08-23 10:36:09 +0200

    login veriif method form

--- src/php/admin/treatmentLogin.php
+++ src/php/admin/treatmentLogin.php
@@ -1,5 +1,11 @@
 <?php
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    $_SESSION['error-message'] = "Une erreur est survenue";
+    header('Location: index.php?action=Connexion');
+    exit;
+}
+
 // Valider et nettoyer les entrées utilisateur
 $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 $username = htmlspecialchars(strip_tags(trim($_POST['username'])));
@@ -15,7 +21,7 @@ if (empty($username) || empty($password)) {
     }
 }
 
-if (empty($captcha)){
+if (empty($captcha)) {
     $_SESSION['error-message'] = "Erreur de vérification du captcha.";
     header('Location: index.php?action=Connexion');
     exit;
@@ -53,11 +59,11 @@ function validation_google_captcha($captch_response)
     );
     $verify = curl_init();
     curl_setopt($verify, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-        curl_setopt($verify, CURLOPT_POST, true);
-        curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($verify, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($verify);
+    curl_setopt($verify, CURLOPT_POST, true);
+    curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($verify, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($verify);
     $response = json_decode($response, true);
     $error_message = '';
     if (isset($response['error-codes']) && !empty($response['error-codes'])) {
